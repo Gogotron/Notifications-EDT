a30ad8d0d26751e86bab3d3101a2b440ad22eedc
Gogotron <ggtrnteam@gmail.com>
2021-06-09 17:17:08 +0200

    Cleaned up urls.

--- emploi_du_temps.py
+++ emploi_du_temps.py
@@ -1,20 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from helper_functs import real_date, timeint, sort_schedule, remove_non_PI
-
-BASE_URL = "https://apogee.u-bordeaux.fr/edt-st/Licence1/Semestre2/"
-URL_MINF_A3	  = BASE_URL+"g254484.xml"
-URL_MINF_A5   = BASE_URL+"g254486.xml"
-URL_MINF_B5   = BASE_URL+"g301648.xml"
-URL_PYC_A3    = BASE_URL+"g303429.xml"
-URL_CH_A2     = BASE_URL+"g254353.xml"
-URL_SI_A1     = BASE_URL+"g302805.xml"
-URL_SV_A1     = BASE_URL+"g299833.xml"
-
-URL_MINF_A    = BASE_URL+"g254481.xml"
-URL_MINF_B    = BASE_URL+"g301643.xml"
-URL_CMI_ISI   = BASE_URL+"g301653.xml"
-URL_CMI_OPTIM = BASE_URL+"g347963.xml"
+from urls import PI_URLS, MI_URLS
 
 def get_schedule(URL):
 	page = requests.get(URL)
@@ -71,7 +58,7 @@ def parse_events(events):
 
 	return sorted_schedule(event_dictionnaries)
 
-def fetch_combined(urls,PI_filter=False):
+def fetch_combined(urls, PI_filter=False):
 	combined_schedule = []
 	for s in map(get_schedule,urls):
 		for e in remove_non_PI(s) if PI_filter else s:
@@ -83,14 +70,6 @@ if __name__=="__main__":
 	from helper_functs import print_event
 	import time
 
-	urls = (
-		URL_MINF_A3,
-		URL_MINF_A5,
-		URL_MINF_B5,
-		URL_PYC_A3,
-		URL_CH_A2,
-		URL_SI_A1)
-
 	start = time.time()
-	c = fetch_combined(urls,PI_filter=True)
+	sche = fetch_combined(PI_URLS,PI_filter=True)
 	print("Time:",time.time()-start)
