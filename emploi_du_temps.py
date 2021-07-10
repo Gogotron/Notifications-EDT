897b433404727705a462952a9e448e62b03f36ca
Gogotron <ggtrnteam@gmail.com>
2021-07-10 09:43:12 +0200

    Refactored `parse_events` into 2 separate functions.

--- emploi_du_temps.py
+++ emploi_du_temps.py
@@ -19,57 +19,59 @@ def get_schedule(url: str) -> list:
 def parse_events(events: list) -> list:
 	event_dictionnaries = []
 	for event in events:
-		edict = {}
-
-		# Mandatory attribute
-		edict["date"] = event["date"]
-		# Mandatory tag
-		edict["day"]        = event.day        .get_text()
-		edict["starttime"]  = event.starttime  .get_text()
-		edict["endtime"]    = event.endtime    .get_text()
-		edict["category"]   = event.category   .get_text()
-		edict["prettyweek"] = event.prettyweeks.get_text()
-		# Optional values
-		if event.resources.module:
-			edict["module"] = event.resources.module.item.get_text()
-		else:
-			edict["module"] = None
-		if event.resources.room:
-			edict["room"] = event.resources.room.item.get_text()
-		else:
-			edict["room"] = None
-		if event.notes:
-			for br in event.notes.find_all('br'):
-				br.replace_with("###")
-			edict["notes"] = event.notes.get_text().replace("###", " ").strip()
-		else:
-			edict["notes"] = None
-		# Optional list values
-		if event.resources.group:
-			edict["groups"] = [
-				i.get_text()
-				for i in event.resources.group.find_all('item')
-			]
-		else:
-			edict["groups"] = None
-		if event.resources.staff:
-			edict["staff"] = [
-				i.get_text()
-				for i in event.resources.staff.find_all('item')
-			]
-		else:
-			edict["staff"] = None
-
-		event_dictionnaries.append(edict)
-
-	for event in event_dictionnaries:
-		event["week"]      = int(event["prettyweek"])
-		event["start"]     = tuple(map(int, event["starttime"].split(':')))
-		event["end"]       = tuple(map(int, event["endtime"].split(':')))
-		event["real_date"] = real_date(event["date"], event["day"])
-		event["timeint"]   = event_timeint(event)
-
+		event_dictionnaries.append(parse_event(event))
 	return sorted_schedule(event_dictionnaries)
+
+
+def parse_event(event) -> dict:
+	edict = {}
+
+	# Mandatory attribute
+	edict["date"] = event["date"]
+	# Mandatory tag
+	edict["day"]        = event.day        .get_text()
+	edict["starttime"]  = event.starttime  .get_text()
+	edict["endtime"]    = event.endtime    .get_text()
+	edict["category"]   = event.category   .get_text()
+	edict["prettyweek"] = event.prettyweeks.get_text()
+	# Optional values
+	if event.resources.module:
+		edict["module"] = event.resources.module.item.get_text()
+	else:
+		edict["module"] = None
+	if event.resources.room:
+		edict["room"] = event.resources.room.item.get_text()
+	else:
+		edict["room"] = None
+	if event.notes:
+		for br in event.notes.find_all('br'):
+			br.replace_with("###")
+		edict["notes"] = event.notes.get_text().replace("###", " ").strip()
+	else:
+		edict["notes"] = None
+	# Optional list values
+	if event.resources.group:
+		edict["groups"] = [
+			i.get_text()
+			for i in event.resources.group.find_all('item')
+		]
+	else:
+		edict["groups"] = None
+	if event.resources.staff:
+		edict["staff"] = [
+			i.get_text()
+			for i in event.resources.staff.find_all('item')
+		]
+	else:
+		edict["staff"] = None
+
+	edict["week"]      = int(edict["prettyweek"])
+	edict["start"]     = tuple(map(int, edict["starttime"].split(':')))
+	edict["end"]       = tuple(map(int, edict["endtime"].split(':')))
+	edict["real_date"] = real_date(edict["date"], edict["day"])
+	edict["timeint"]   = event_timeint(edict)
+
+	return edict
 
 
 def fetch_combined(urls: list, PI_filter: bool = False) -> list:
